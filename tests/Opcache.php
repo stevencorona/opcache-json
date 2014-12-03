1ca27808899ce6b15f4a471be74cb3926b259cac
Steve Corona <thestevecorona@gmail.com>
2014-12-03 09:08:03 -0800

    test that we can construct the object

--- tests/Opcache.php
+++ tests/Opcache.php
@@ -5,4 +5,9 @@ class OpcacheTest extends PHPUnit_Framework_TestCase {
 		$loaded = extension_loaded("Zend OPcache");
 		$this->assertTrue($loaded);
 	}
+
+	public function testCanInitialize() {
+		$opcache = new Opcache\Status;
+		$this->assertInstanceOf('Status', $opcache);
+	}
 }
