c7ffc29eabf6814beb19008bbd24ca5e5e06dabd
Steve Corona <thestevecorona@gmail.com>
2014-12-04 07:09:30 -0800

    test no scripts without statsd

--- tests/Opcache.php
+++ tests/Opcache.php
@@ -8,6 +8,24 @@ class OpcacheTest extends PHPUnit_Framework_TestCase {
 
 	public function testCanInitialize() {
 		$opcache = new Opcache\Status;
-		$this->assertInstanceOf('Status', $opcache);
+		$this->assertInstanceOf('Opcache\Status', $opcache);
+	}
+
+	public function testInitializeStatsdOptions() {
+
+	}
+
+	public function testInitializeStatsdBlock() {
+
+	}
+
+	public function testStatusNoScriptsNoStatsd() {
+		$opcache = new Opcache\Status;
+		$result = $opcache->status();
+
+		$data = json_decode($result);
+
+		$this->assertEquals(JSON_ERROR_NONE, json_last_error());
+		$this->assertContains("status", $data);
 	}
 }
