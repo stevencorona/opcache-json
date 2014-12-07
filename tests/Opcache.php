0dcf771062e801dcefc1c73d493b9b28852f308e
Steve Corona <thestevecorona@gmail.com>
2014-12-07 10:43:44 -0800

    add tests for checking output and scripts

--- tests/Opcache.php
+++ tests/Opcache.php
@@ -26,6 +26,82 @@ class OpcacheTest extends PHPUnit_Framework_TestCase {
 		$data = json_decode($result);
 
 		$this->assertEquals(JSON_ERROR_NONE, json_last_error());
-		$this->assertContains("status", $data);
+
+		$this->assertInstanceOf('stdClass', $data);
+		$this->assertObjectHasAttribute("status", $data);
+
+		// Check for all top level keys
+		$this->assertObjectHasAttribute("opcache_enabled",        $data->status);
+		$this->assertObjectHasAttribute("cache_full",             $data->status);
+		$this->assertObjectHasAttribute("restart_pending",        $data->status);
+		$this->assertObjectHasAttribute("restart_in_progress",    $data->status);
+		$this->assertObjectHasAttribute("memory_usage",           $data->status);
+		$this->assertObjectHasAttribute("interned_strings_usage", $data->status);
+		$this->assertObjectHasAttribute("opcache_statistics",     $data->status);
+
+		$this->assertFalse(isset($data->scripts));
+	}
+
+	public function testStatusScriptsNoStatsd() {
+		$opcache = new Opcache\Status;
+		$result = $opcache->status(true);
+
+		$data = json_decode($result);
+
+		$this->assertEquals(JSON_ERROR_NONE, json_last_error());
+
+		$this->assertInstanceOf('stdClass', $data);
+		$this->assertObjectHasAttribute("status", $data);
+
+		// Check for all top level keys
+		$this->assertObjectHasAttribute("opcache_enabled",        $data->status);
+		$this->assertObjectHasAttribute("cache_full",             $data->status);
+		$this->assertObjectHasAttribute("restart_pending",        $data->status);
+		$this->assertObjectHasAttribute("restart_in_progress",    $data->status);
+		$this->assertObjectHasAttribute("memory_usage",           $data->status);
+		$this->assertObjectHasAttribute("interned_strings_usage", $data->status);
+		$this->assertObjectHasAttribute("opcache_statistics",     $data->status);
+
+		$this->assertTrue(isset($data->scripts));
+
+		$this->assertArrayHasKey(0, $data->scripts);
+
+		// Check for all script level keys
+		$this->assertObjectHasAttribute("full_path",           $data->scripts[0]);
+		$this->assertObjectHasAttribute("hits",                $data->scripts[0]);
+		$this->assertObjectHasAttribute("memory_consumption",  $data->scripts[0]);
+		$this->assertObjectHasAttribute("last_used",           $data->scripts[0]);
+		$this->assertObjectHasAttribute("last_used_timestamp", $data->scripts[0]);
+		$this->assertObjectHasAttribute("timestamp",           $data->scripts[0]);
+	}
+
+	public function testScriptsSortedByMemory() {
+		$opcache = new Opcache\Status;
+		$result = $opcache->status(true);
+
+		$data = json_decode($result);
+
+		$this->assertEquals(JSON_ERROR_NONE, json_last_error());
+		$this->assertTrue(isset($data->scripts));
+
+		$last_memory = 99999999999;
+
+		foreach($data->scripts as $script) {
+			$this->assertGreaterThanOrEqual($script->memory_consumption, $last_memory);
+			$last_memory = $script->memory_consumption;
+		}
+
+	}
+
+	public function testStatsdOptions() {
+
+	}
+
+	public function testNoStatsd() {
+
+	}
+
+	public function testStatsdBlock() {
+
 	}
 }
